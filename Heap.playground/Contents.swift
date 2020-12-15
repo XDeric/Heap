9887dc3ae058989f5b5f2683ef7a53ec1d761c8f
XDEric <eeeric3995@yahoo.com>
2020-12-15 11:16:02 -0500

    add insert function that will adhere it min Heap criteria

--- Heap.playground/Contents.swift
+++ Heap.playground/Contents.swift
@@ -16,6 +16,10 @@ import Cocoa
 struct Heap{
     private var nodes = [2,8,10,16,21,30,36]
     
+    private var orderingCriteria: (Int, Int) -> Bool
+    public init(_ sort: @escaping (Int, Int) -> Bool) {
+        self.orderingCriteria = sort
+      }
     
     public func parentIndex(_ index: Int)-> Int{
         return (index - 1) / 2
@@ -28,16 +32,39 @@ struct Heap{
     }
     
     public func pValue(_ index: Int)-> Int?{
+        guard parentIndex(index) > 0 else {return nil}
         return nodes[parentIndex(index)]
     }
     public func lValue(_ index: Int)-> Int?{
+        guard leftIndex(index) < nodes.count else {return nil}
         return nodes[leftIndex(index)]
     }
     public func rValue(_ index: Int)-> Int?{
+        guard rightIndex(index) > nodes.count else {return nil}
         return nodes[rightIndex(index)]
+    }
+    
+    public mutating func insert(_ item: Int){
+        nodes.append(item)
+        shiftUp(nodes.count - 1)
+    }
+    
+    public mutating func shiftUp(_ index: Int){
+        let child = nodes[index]
+        var childIndex = index
+        var parentIndex = self.parentIndex(childIndex)
+        
+        while childIndex > 0 && orderingCriteria(child, nodes[parentIndex]) {
+            
+            nodes[childIndex] = nodes[parentIndex]
+            childIndex = parentIndex
+            parentIndex = self.parentIndex(childIndex)
+            
+        }
+            nodes[childIndex] = child
     }
 }
 
-let minHeap = Heap()
+var minHeap = Heap(<)
 
-print(minHeap.pValue(1))
+print(minHeap.pValue(1)!)
